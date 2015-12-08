88645120a94da27feeaa11bd1c30deea938d576f
Daniel Kaufhold <daniel.kaufhold@bitmazk.com>
2015-12-08 09:40:09 +0100

    Excluded constants from coverage

--- server_guardian/constants.py
+++ server_guardian/constants.py
@@ -1,14 +1,14 @@
 """Collection of constants fo the ``server_guardian`` app."""
 
 
-SERVER_STATUS = {
+SERVER_STATUS = {  # pragma: no cover
     'OK': 'OK',
     'WARNING': 'WARNING',
     'DANGER': 'DANGER',
 }
 
 
-HTML_STATUS_FALLBACK = {
+HTML_STATUS_FALLBACK = {  # pragma: no cover
     'status': SERVER_STATUS['DANGER'],
     'info': 'JSON response could not be decoded.'
 }
